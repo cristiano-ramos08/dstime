8cd517d748381f369545e4b4baddb018197f0309
Cristiano Ramos <limaramos.c@gmail.com>
2022-01-21 12:01:12 -0300

    Project created

---
+++ backend/src/main/java/com/racajovem/dstime/DstimeApplication.java
@@ -0,0 +1,13 @@
+package com.racajovem.dstime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DstimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DstimeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/racajovem/dstime/DstimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.racajovem.dstime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DstimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
